7da5d0838897da99872e5cfc3386608e1c5d6bd7
Alex <alexandre.charlot@wisembly.com>
2018-04-17 19:19:56 +0200

    fixing react hmr

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './components/Home'
 import './assets/style/application.scss'
 
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
-)
+const render = () => {
+  // NB: We have to re-require MyApp every time or else this won't work
+  // We also need to wrap our app in the AppContainer class
+  ReactDOM.render(<Home />, document.getElementById('root'))
+}
+
+render()
+if (module.hot) module.hot.accept(render)
